eba2ae1b0312daaabf596c0ae2ef00ac15efc349
Ankush Jagga <ankushjagga22@gmail.com>
2022-09-10 11:23:13 +0530

    make it better

--- server/connection/db.js
+++ server/connection/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI).then(()=>{
+mongoose.connect("mongodb+srv://ankushjagga:9876054247@cluster0.jzjho9x.mongodb.net/Recipes?retryWrites=true&w=majority").then(()=>{
     console.log("connection susssful");
 }).catch((err)=>{
     console.log("something went wrong" + err);
